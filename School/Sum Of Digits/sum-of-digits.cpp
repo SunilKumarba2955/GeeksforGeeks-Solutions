8e7d8f7409d25aa94f3cbc37c7e0b8240736cd80
Sunil Kumar B A <90314405+SunilKumarba2955@users.noreply.github.com>
2023-11-15 04:01:35 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum Of Digits/sum-of-digits.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int sumOfDigits(int n){
+        //code here
+        if(n==0)
+           return 0;
+        return n%10 + sumOfDigits(n/10);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.sumOfDigits(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
