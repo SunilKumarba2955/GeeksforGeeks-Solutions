08dd6954750cf474ef8ae4f4864243fe1a8db714
Sunil Kumar B A <90314405+SunilKumarba2955@users.noreply.github.com>
2024-02-27 19:54:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Play With OR/play-with-or.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    def game_with_number (self, arr,  n) : 
+        #Complete the function
+        return [ arr[i]|arr[i+1] for i in range(n-1)]+[arr[n-1]]
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    ob = Solution()
+    res = ob.game_with_number(arr, n);
+    print(*res)
+
+
+
+
+# } Driver Code Ends
